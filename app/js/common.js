5cd07c6d50825700c7ca5d972f48f745467ec33e
Lesnjak <vnachalesobaka@gmail.com>
2018-07-27 17:56:12 +0300

    '27.07.2018'

--- app/js/common.js
+++ app/js/common.js
@@ -5,125 +5,106 @@ $('document').ready(function () {
                 $(b).addClass('while-move');
             }
         })
+    });
+    var res1 = $('.main-head').height();
+    $(document).scroll(function () {
+        // if()
+        if($(document).scrollTop()>res1){
+            $('.main-head').addClass('scroll-on');
+
+        }else {
+            $('.main-head').removeClass('scroll-on');
+        }
+
+
+
+    })
+    /* ===== Logic for creating fake Select Boxes ===== */
+    $('.sel').each(function() {
+        $(this).children('select').css('display', 'none');
+
+        var $current = $(this);
+
+        $(this).find('option').each(function(i) {
+            if (i == 0) {
+                $current.prepend($('<div>', {
+                    class: $current.attr('class').replace(/sel/g, 'sel__box')
+                }));
+
+                var placeholder = $(this).text();
+                $current.prepend($('<span>', {
+                    class: $current.attr('class').replace(/sel/g, 'sel__placeholder'),
+                    text: placeholder,
+                    'data-placeholder': placeholder
+                }));
+
+                return;
+            }
+
+            $current.children('div').append($('<span>', {
+                class: $current.attr('class').replace(/sel/g, 'sel__box__options'),
+                text: $(this).text()
+            }));
+        });
+    });
+
+// Toggling the `.active` state on the `.sel`.
+    $('.sel').click(function() {
+        $(this).toggleClass('active');
+    });
+
+// Toggling the `.selected` state on the options.
+    $('.sel__box__options').click(function() {
+        var txt = $(this).text();
+        var index = $(this).index();
+
+        $(this).siblings('.sel__box__options').removeClass('selected');
+        $(this).addClass('selected');
+
+        var $currentSel = $(this).closest('.sel');
+        $currentSel.children('.sel__placeholder').text(txt);
+        $currentSel.children('select').prop('selectedIndex', index + 1);
+    });
+
+    $('.check-box label input:checked').parent().addClass('checked');
+    $('.check-box label input').click(function () {
+        $('.check-filter label input').parent().removeClass('checked');
+        if($(this).is(':checked')){
+            $(this).parent().addClass('checked');
+        }else{
+            $(this).parent().removeClass('checked');
+        }
 
     });
 
-
-
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav',
-        adaptiveHeight: true
-    });
-    $('.slider-nav').slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        dots: true,
-        prevArrow:'<button class="prev"></button>',
-        nextArrow:'<button class="next"></button>',
-        focusOnSelect: true,
-        centerPadding: '40px',
+    $('.slider').slick({
+        centerMode: true,
+        slidesToShow: 3,
+        infinite: true,
         arrows: true,
         responsive: [
             {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow:2
-                }
-            },
-            {
-                breakpoint: 1062,
+                breakpoint: 993,
                 settings: {
                     slidesToShow: 1,
-
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    arrows: false,
-                    vertical: true
                 }
             }
 
-        ]
-    });
-    $('.big').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: true,
-        asNavFor: '.small',
-        adaptiveHeight: true,
-        prevArrow:'<button class="prev1"></button>',
-        nextArrow:'<button class="next1"></button>',
-        responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-
-                }
-            },
-            {
-                breakpoint: 1062,
-                settings: {
-
-
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: false
-
-                }
-            }
 
         ]
+
+
+
     });
-    $('.small').slick({
-        slidesToShow:5,
-        slidesToScroll: 1,
-        asNavFor: '.big',
-        dots: false,
-        focusOnSelect: true,
-        centerPadding: '40px',
-        arrows: false,
-        responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow:5
-                }
-            },
-            {
-                breakpoint: 1062,
-                settings: {
-                    slidesToShow: 4,
+    $('.slick-arrow').click(function () {
+        console.log("---",555);
+        var clone = $(".slick-current .text-box").clone();
+        $(".clone").html(clone);
+    })
+    var clone = $(".slick-current .text-box").clone();
+        $(".clone").html(clone);
 
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 3,
 
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 2,
-
-                }
-            }
-
-        ]
-    });
     $('.button-crest').click(function () {
         $('.thank-you').hide();
 
@@ -132,6 +113,21 @@ $('document').ready(function () {
     $('[data-form]').click(function () {
         $('.thank-you-form').show();
     });
+
+    var i = 1;
+    var buttonDel = ' <a class="delete"> <div></div> <div></div> </a>';
+    $('.add-form').click(function () {
+
+        var cloneForm = $(".select-box .form-section:first-child").clone().append(buttonDel) ;
+        $(".select-box").append(cloneForm);
+        i+=1;
+        $('.delete').click(function () {
+            $(this).parent().remove();
+        })
+
+
+    });
+
 
     ///////////////// mail////////////////////
 
